d1603e94a2a55d396eb9ad2072581adf391603fe
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2021-10-28 12:47:44 +0600

    Test case checking...

--- Python/DateTimeCalculator.py
+++ Python/DateTimeCalculator.py
@@ -1,7 +1,10 @@
 # test case checking:
 """
-case 1: month same, date different. (27/11/2021, 29/11/2021)
-case 2: 
+Case 1: month same, date same, year same. (27/11/2021, 27/11/2021)
+Case 2: month same, date same, year different. (27/11/2021, 27/11/2022)
+Case 3: month different, date different, year same. (15/7/2021, 29/11/2021)
+Case 4: month same, date different, year same. (27/11/2021, 29/11/2021)
+Case 5: month different, date same, year same. (27/11/2021, 27/12/2021)
 """
 
 
@@ -56,15 +59,15 @@ def totalDays(start, end):
     monthSerial = startingMonth
 
 
-    if startingMonth == endingMonth:
+    if startingMonth == endingMonth and startingYear == endingYear:
 
         # including both starting and ending dates
-        totalDayCount += (endingDate - startingDate) + 1
+        totalDayCount += (endingDate - startingDate)
 
     else:
 
         # adding the first and last months' days
-        totalDayCount += endingDate + (monthList[startingMonth-1]-startingDate) + 1
+        totalDayCount += endingDate + (monthList[startingMonth-1]-startingDate)
 
 
         # counting days in between the starting and ending months
@@ -80,7 +83,7 @@ def totalDays(start, end):
             if monthSerial == 1 and leapYear(currentYear) == True:
                 totalDayCount += 1
 
-            print("MONTH: ", monthSerial, "__YEAR__: ", currentYear)
+            # print("MONTH: ", monthSerial, "__YEAR__: ", currentYear)
 
             if monthSerial == endingMonth - 1 and currentYear == endingYear:
                 break
@@ -91,44 +94,6 @@ def totalDays(start, end):
 
             # increment
             monthSerial += 1
-       
-
-
-    # conditions checking
-    # if startingYear == endingYear:
-
-        
-
-    # elif startingYear != endingYear:
-
-    #     # adding the first and last months' days (including BOTH start and end Date)
-    #     totalDayCount += endingDate + \
-    #         (monthList[startingMonth-1]-startingDate) + 1
-
-    #     # counting days in between the starting and ending months
-
-    #     while True:
-
-    #         monthSerial = monthSerial % 12
-
-    #         # check for new year
-    #         if monthSerial == 0:
-    #             currentYear += 1
-
-    #         # check for leap year
-    #         if monthSerial == 1 and leapYear(currentYear) == True:
-    #             totalDayCount += 1
-
-    #         # extracting days from ith month
-    #         days = monthList[monthSerial]
-    #         totalDayCount += days
-
-    #         # increment
-    #         monthSerial += 1
-
-    #         if monthSerial == endingMonth - 1 and currentYear == endingYear:
-    #             break
-
 
     return totalDayCount
 
@@ -138,4 +103,4 @@ startDate = input("Enter starting date (DD/MM/YYYY): ").split("/")
 endDate = input("Enter ending date (DD/MM/YYYY): ").split("/")
 
 count = totalDays(startDate, endDate)
-print(count)
+print("\nTotal Days: ", count, "days (Excluding End day)\n")
